28ff173576280675fadd34488f6345c93f9f30a5
Daniel Gurvič <gurvic.danielius@gmail.com>
2024-09-28 02:57:35 +0300

    Nuskaitymas

--- Floido-Varsalo algoritmas.py
+++ Floido-Varsalo algoritmas.py
@@ -6,7 +6,23 @@ import pandas as pd
 sys.stdout.reconfigure(encoding='utf-8')
 
 import pandas as pd
-
+def nuskaitymas(filepath):
+    zodynas = {} 
+    with open(file_path, 'r') as file:
+        for line in file:
+            airport, connections = line.split(':')
+            airport = airport.strip()  
+            connections = connections.strip().split(',')  
+            
+     
+            flights = []
+            for connection in connections:
+                destination, price = connection.split()  
+                flights.append((destination, int(price)))  
+            
+            zodynas[airport] = flights
+    
+    return zodynas
 def floyd_warshall(graph):
     virsuniu_skaicius=len(graph)
     atstumai = np.full((virsuniu_skaicius, virsuniu_skaicius), np.inf)
@@ -22,6 +38,7 @@ def floyd_warshall(graph):
                 atstumai[i][j] = min(atstumai[i][j], atstumai[i][k] + atstumai[k][j])                
     return atstumai;
 
+   
 
 #Sukuriam objektus: virsunes
 virsune1 = str.Virsune(1) #sukuriam viršūnę su id 1
@@ -36,8 +53,9 @@ virsune4.add_edge(virsune2, -1)
 virsune2.add_edge(virsune3, 3) 
 virsune2.add_edge(virsune1, 4) 
 
- 
-print(virsune1.edges[3]) #spausdinam krastinę iš viršūnės 1 į 3
+# print(virsune2) 
+# print(virsune1.edges[3]) #spausdinam krastinę iš viršūnės 1 į 3
 graph = [virsune1, virsune2, virsune3, virsune4]
 atstumai = floyd_warshall(graph)
-print(atstumai)
+file_path ="C:/Users/danie/Downloads/Airport_Distance_Dataset.csv"
+print(nuskaitymas(file_path))
